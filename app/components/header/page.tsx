1c75e47d6dbff600ff2799454afe4d9c386114e9
Uditya <workforuditya@gmail.com>
2024-03-25 01:46:31 +0530

    chore: minor ui changes

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -4,32 +4,31 @@ import logo from "@/assets/logo.png";
 import Image from "next/image";
 import { GoPencil } from "react-icons/go";
 import { IoReaderOutline } from "react-icons/io5";
-import { FaRegUser } from "react-icons/fa";
 import { UserButton } from "@clerk/nextjs";
 
 function Header() {
   return (
-    <div className="flex flex-row gap-x-3 justify-between items-center px-4  backdrop-blur-md bg-opacity-[25%] w-full ">
+    <div className="flex flex-row gap-x-3 justify-between items-center px-4  backdrop-blur-md bg-opacity-[25%] w-[80vw] ">
       <div>
         <Link href={"/homescreen"} className="items-center">
-          <Image src={logo} alt="" width={100} height={10} />
+          <Image src={logo} alt="" width={120} height={15} />
         </Link>
       </div>
-      <div className="text-2xl flex flex-row gap-x-5 items-center">
-        <Link href={"/blogscreen"} >
-          <div className="flex flex-row items-center gap-x-2">
-            Blogs <IoReaderOutline />
+      <div className="text-2xl flex flex-row gap-x-5 items-center h">
+        <Link href={"/blogscreen"} className="hover:text-teal-200" >
+          <div className="flex flex-row items-center gap-x-2 ">
+             <IoReaderOutline size={35} />
           </div>{" "}
         </Link>
-        <Link href={"/createpost"}   className="flex flex-row">
+        <Link href={"/createpost"}   className="flex flex-row hover:text-orange-300">
           <div className="flex flex-row items-center gap-x-2">
-            Create <GoPencil />
+           <GoPencil size={35}/>
           </div>
         </Link>
         <Link href={""}>
           {" "}
-          <div>
-            <UserButton/>
+          <div className="">
+            <UserButton appearance={{ elements: { userButtonAvatarBox: { height: '50px', width: '50px', }, }, }}/>
           </div>
         </Link>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header/page";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
  
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,10 +24,13 @@ export default function RootLayout({
       className="bg-gray-800 text-white "
     >
       <body className={inter.className}>
-        <ClerkProvider>
-        
+        <ClerkProvider appearance={{
+        baseTheme: dark
+      }} >
+        <div className="flex flex-col items-center m-auto justify-between">
            <Header />
           {children}
+        </div>
         </ClerkProvider>
        </body>
     </html>
