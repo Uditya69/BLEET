fdfec9cd14bc994c5dc1771d685fbbac4bcc42c4
uditya69 <uditya04@gmail.com>
2024-11-07 00:40:55 +0530

    fix: build errors

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,18 @@ const nextConfig = {
             },
         ],
     },
+    webpack: (config) => {
+        config.module.rules.push({
+          test: /node_modules\/undici\/.*\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        });
+        return config;
+      },
 };
 
 export default nextConfig;
